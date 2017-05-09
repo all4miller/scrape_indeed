591639bce3343d5cfa506e20d3c0eaa27e9cab35
Nick Bair <nickbair@golfingbuddha.com>
2017-05-09 16:23:45 -0400

    Stub out basic UI, closes #1.

--- lib/scrape_indeed/cli.rb
+++ lib/scrape_indeed/cli.rb
@@ -3,5 +3,23 @@ class ScrapeIndeed::CLI
   def call
     ScrapeIndeed::Graphic.display
     puts "Welcome to Scrape Indeed"
+    puts "Type 'scrape' to get started"
+    input = gets.strip.downcase
+
+    if input == "scrape"
+      config_search
+    end
+  end
+
+  def config_search
+    puts "Please enter a city:"
+    city = gets.strip.downcase
+    puts "Please enter a state:"
+    state = gets.strip.downcase
+    puts "Please enter keywords (comma or space separated)"
+    keywords = gets.strip.downcase
+
+    # call scrape with above data
+    puts "Scraping...one moment"
   end
 end
