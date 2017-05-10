2efba80d64a506188e6e0480049b94f87987a980
Nick Bair <nickbair@golfingbuddha.com>
2017-05-10 17:14:55 -0400

    Clean up code in cli.rb

--- lib/scrape_indeed/cli.rb
+++ lib/scrape_indeed/cli.rb
@@ -12,6 +12,7 @@ class ScrapeIndeed::CLI
     if input == "scrape"
       config_search
       scraping
+      post_scrape
     else
       start_menu
     end
@@ -36,39 +37,46 @@ class ScrapeIndeed::CLI
     puts "Writing #{ScrapeIndeed::Job.all.length} results to file"
     puts "Done => #{ScrapeIndeed::Excel.config_filename}"
     puts "\n\n"
+  end
+
+  def post_scrape
     puts "What would you like to do next?"
-    puts "Type:\n1 - Start another scrape\n2 - Print data to console"
+    puts "Type:\n1 - Start another scrape\n2 - See scrape data in console"
     input2 = gets.strip
 
     if input2 == "1"
       start_menu
     elsif input2 == "2"
-      data_detail
+      detail_menu
+    end
+  end
+
+  def detail_menu
+    puts "Type:\nPrint - Print 10 results\nBack - Back to main menu\n"
+    # Line \# - Print out a description"
+    input =  gets.strip.downcase
+
+    if input == "print"
+      print_results(10)
+      # Need another menu after this
+      # Give user option to drill into job or go back
+    elsif input == "back"
+      start_menu
+    elsif /\A\d+\z/.match(input)
+      print_detail(input)
+      start_menu
     end
   end
 
   def print_results(qty)
+    puts "Printing #{qty} results"
     ScrapeIndeed::Job.all.take(qty).each.with_index do |e,i|
       puts "#{i + 1}. #{e.title} @ #{e.company} in #{e.location}."
     end
   end
 
-  def data_detail
-    base_qty = 10
-
-    puts "Printing first 10 results"
-    print_results(base_qty)
-    puts "Type:\nMore - Print 5 more results:\nBack - Back to main menu\nLine \# - Print out a description"
-    input =  gets.strip.downcase
-
-    if input == "more"
-      print_results(base_qty + 5)
-    elsif input == "back"
-      start_menu
-    elsif /\A\d+\z/.match(input)
-      item = ScrapeIndeed::Job.all[input.to_i - 1]
-      puts "#{item.title} @ #{item.company} in #{item.location}, #{item.description}"
-      start_menu
-    end
+  def print_detail(input)
+    item = ScrapeIndeed::Job.all[input.to_i - 1]
+    puts "#{item.title} @ #{item.company} in #{item.location}, #{item.description}"
   end
 end
